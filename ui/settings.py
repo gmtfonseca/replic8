f734918d53158d18f7fb76e6e83524ffd36aefef
Gustavo Fonseca <gustavofonseca94@gmail.com>
2019-10-29 19:13:21 -0300

    Improve settings UI

--- ui/settings.py
+++ ui/settings.py
@@ -9,34 +9,32 @@ def show(parent):
 class SettingsDialog(wx.Dialog):
 
     def __init__(self, parent, title):
-        super().__init__(parent=parent, title=title, size=(500, 350))
+        super().__init__(parent=parent, title=title, size=(500, 360))
         self._initLayout()
 
     def _initLayout(self):
         panel = wx.Panel(self)
         # panel.SetBackgroundColour(wx.YELLOW)
 
-        lblInterval = wx.StaticText(panel, -1, 'Intervalo (dias)')
-        self.txtInterval = wx.TextCtrl(panel, -1, '')
+        lblInterval = wx.StaticText(panel, -1, 'Intervalo (dias)', size=(90, -1))
+        self.txtInterval = wx.TextCtrl(panel, -1, '', size=(50, -1))
 
-        lblDest = wx.StaticText(panel, -1, 'Pasta de destino')
+        lblDest = wx.StaticText(panel, -1, 'Pasta de destino', size=(90, -1))
         self.txtDest = wx.TextCtrl(panel, -1, '')
         self.btnDest = wx.Button(panel, -1, 'Selecionar', (50, 50))
 
-        lblSource = wx.StaticText(panel, -1, 'Origem')
-        self.txtSource = wx.TextCtrl(panel, -1, '')
-        self.btnSource = wx.Button(panel, -1, 'Adicionar arquivo', (50, 50))
+        self.btnAddSource = wx.Button(panel, -1, 'Adicionar', (50, 50))
+        self.btnRemoveSource = wx.Button(panel, -1, 'Remover', (50, 50))
         self.listSources = wx.ListCtrl(panel, -1,
                                        style=wx.LC_REPORT
-                                       | wx.BORDER_NONE
                                        | wx.LC_EDIT_LABELS
                                        )
 
         self.listSources.InsertColumn(0, "Caminho")
         self.listSources.SetColumnWidth(0, wx.LIST_AUTOSIZE_USEHEADER)
 
-        self.btnOk = wx.Button(panel, -1, 'OK', (50, 50))
-        self.btnCancel = wx.Button(panel, -1, 'Cancelar', (50, 50))
+        self.btnOk = wx.Button(panel, -1, 'OK', (50, -1))
+        self.btnCancel = wx.Button(panel, -1, 'Cancelar', (50, -1))
 
         intervalSizer = wx.BoxSizer(wx.HORIZONTAL)
         intervalSizer.Add(lblInterval)
@@ -44,22 +42,21 @@ class SettingsDialog(wx.Dialog):
 
         destSizer = wx.BoxSizer(wx.HORIZONTAL)
         destSizer.Add(lblDest)
-        destSizer.Add(self.txtDest, wx.SizerFlags(0).Expand().Border(wx.LEFT, 5))
+        destSizer.Add(self.txtDest, wx.SizerFlags(1).Border(wx.LEFT, 5))
         destSizer.Add(self.btnDest, wx.SizerFlags(0).Border(wx.LEFT, 5))
 
         sourceSizer = wx.BoxSizer(wx.HORIZONTAL)
-        sourceSizer.Add(lblSource)
-        sourceSizer.Add(self.txtSource, wx.SizerFlags(0).Border(wx.LEFT, 5))
-        sourceSizer.Add(self.btnSource, wx.SizerFlags(0).Border(wx.LEFT, 5))
+        sourceSizer.Add(self.btnAddSource)
+        sourceSizer.Add(self.btnRemoveSource, wx.SizerFlags(0).Border(wx.LEFT, 5))
 
         footerSizer = wx.BoxSizer(wx.HORIZONTAL)
         footerSizer.Add(self.btnOk)
         footerSizer.Add(self.btnCancel, wx.SizerFlags(0).Border(wx.LEFT, 5))
 
         mainSizer = wx.BoxSizer(wx.VERTICAL)
-        mainSizer.Add(intervalSizer, wx.SizerFlags(0).Expand())
+        mainSizer.Add(intervalSizer)
         mainSizer.Add(destSizer, wx.SizerFlags(0).Expand().Border(wx.TOP, 10))
-        mainSizer.Add(sourceSizer, wx.SizerFlags(0).Expand().Border(wx.TOP, 10))
+        mainSizer.Add(sourceSizer, wx.SizerFlags(0).Border(wx.TOP, 10))
         mainSizer.Add(self.listSources, wx.SizerFlags(0).Expand().Border(wx.TOP, 10))
         mainSizer.Add(footerSizer, wx.SizerFlags(0).Right().Border(wx.TOP, 10))
 
@@ -67,6 +64,23 @@ class SettingsDialog(wx.Dialog):
         panelSizer.Add(mainSizer, wx.SizerFlags(0).Expand().Border(wx.ALL, 10))
 
         panel.SetSizer(panelSizer)
+
+    def createDestDialog(self):
+        dlg = wx.DirDialog(self, "Selecione uma pasta:",
+                           style=wx.DD_DEFAULT_STYLE
+                           # | wx.DD_DIR_MUST_EXIST
+                           # | wx.DD_CHANGE_DIR
+                           )
+        return dlg
+
+    def createSourceDialog(self):
+        dlg = wx.FileDialog(
+            self, message="Selecione um arquivo",
+            style=wx.FD_OPEN | wx.FD_MULTIPLE |
+            wx.FD_CHANGE_DIR | wx.FD_FILE_MUST_EXIST |
+            wx.FD_PREVIEW
+        )
+        return dlg
 
     def quit(self):
         self.Destroy()
@@ -83,8 +97,38 @@ class SettingsPresenter:
         interactor.Install(self, self._view)
         self._view.start()
 
+    def selectDest(self):
+        dialog = self._view.createDestDialog()
+        if dialog.ShowModal() == wx.ID_OK:
+            self._view.txtDest.SetValue(dialog.GetPath())
+
+    def selectSource(self):
+        dialog = self._view.createSourceDialog()
+        if dialog.ShowModal() == wx.ID_OK:
+            self.insertSource(dialog.GetPath())
+
+    def insertSource(self, path):
+        self._view.listSources.InsertItem(self._view.listSources.GetItemCount(), path)
+
+    def removeSource(self):
+        idx = self._view.listSources.GetFirstSelected()
+        print(idx)
+
 
 class SettingsInteractor:
     def Install(self, presenter, view):
         self._presenter = presenter
         self._view = view
+
+        self._view.btnDest.Bind(wx.EVT_BUTTON, self.OnSelectDest)
+        self._view.btnAddSource.Bind(wx.EVT_BUTTON, self.OnSelectSource)
+        self._view.btnRemoveSource.Bind(wx.EVT_BUTTON, self.OnRemoveSource)
+
+    def OnSelectDest(self, evt):
+        self._presenter.selectDest()
+
+    def OnSelectSource(self, evt):
+        self._presenter.selectSource()
+
+    def OnRemoveSource(self, evt):
+        self._presenter.removeSource()
