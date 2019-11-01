bc5cc5d34381db34a2e8bfbfe2d46e4cc2cf6463
Gustavo Fonseca <gustavofonsec94@gmail.com>
2019-11-01 10:44:24 -0300

    Change icons

--- ui/settings.py
+++ ui/settings.py
@@ -31,10 +31,12 @@ class SettingsFrame(wx.Frame):
 
         widthLabel = 95 if sys.platform == 'win32' else 100
 
-        lblInterval = wx.StaticText(panel, -1, 'Intervalo (dias)', size=(widthLabel, -1))
+        lblInterval = wx.StaticText(
+            panel, -1, 'Intervalo (dias)', size=(widthLabel, -1))
         self.txtInterval = wx.TextCtrl(panel, -1, '', size=(50, -1))
 
-        lblDest = wx.StaticText(panel, -1, 'Pasta de destino', size=(widthLabel, -1))
+        lblDest = wx.StaticText(
+            panel, -1, 'Pasta de destino', size=(widthLabel, -1))
         self.txtDest = wx.TextCtrl(panel, -1, '')
         self.txtDest.Disable()
 
@@ -44,10 +46,13 @@ class SettingsFrame(wx.Frame):
         sourceBox = wx.StaticBox(panel, -1, 'Arquivos a serem copiados')
 
         bmpAdd = wx.Bitmap(assets.image('add.png'), wx.BITMAP_TYPE_PNG)
-        self.btnAddSource = wx.BitmapButton(sourceBox, -1, bmpAdd, (50, 50))
+        self.btnAddSource = wx.Button(sourceBox, -1, 'Adicionar', (50, 50))
+        self.btnAddSource.SetBitmap(bmpAdd)
 
         bmpRemove = wx.Bitmap(assets.image('remove.png'), wx.BITMAP_TYPE_PNG)
-        self.btnRemoveSource = wx.BitmapButton(sourceBox, -1, bmpRemove, (50, 50))
+        self.btnRemoveSource = wx.Button(sourceBox, -1, 'Remover', (50, 50))
+        self.btnRemoveSource.SetBitmap(bmpRemove)
+
         self.listSources = wx.ListCtrl(sourceBox, -1,
                                        style=wx.LC_REPORT
                                        )
@@ -60,7 +65,8 @@ class SettingsFrame(wx.Frame):
 
         intervalSizer = wx.BoxSizer(wx.HORIZONTAL)
         intervalSizer.Add(lblInterval)
-        intervalSizer.Add(self.txtInterval, wx.SizerFlags(1).Border(wx.LEFT, 5))
+        intervalSizer.Add(self.txtInterval,
+                          wx.SizerFlags(1).Border(wx.LEFT, 5))
 
         destSizer = wx.BoxSizer(wx.HORIZONTAL)
         destSizer.Add(lblDest)
@@ -69,15 +75,19 @@ class SettingsFrame(wx.Frame):
 
         sourceSizer = wx.BoxSizer(wx.HORIZONTAL)
         sourceSizer.Add(self.btnAddSource)
-        sourceSizer.Add(self.btnRemoveSource, wx.SizerFlags(0).Border(wx.LEFT, 5))
+        sourceSizer.Add(self.btnRemoveSource,
+                        wx.SizerFlags(0).Border(wx.LEFT, 5))
 
         sourceBoxSizer = wx.BoxSizer(wx.VERTICAL)
         borderTop = 15 if sys.platform == 'win32' else 0
-        sourceBoxSizer.Add(sourceSizer, wx.SizerFlags(0).Expand().Border(wx.TOP, borderTop))
-        sourceBoxSizer.Add(self.listSources, wx.SizerFlags(0).Expand().Border(wx.TOP, 5))
+        sourceBoxSizer.Add(sourceSizer, wx.SizerFlags(
+            0).Expand().Border(wx.TOP, borderTop))
+        sourceBoxSizer.Add(self.listSources, wx.SizerFlags(
+            0).Expand().Border(wx.TOP, 5))
 
         sourceBoxSizerOuter = wx.BoxSizer(wx.VERTICAL)
-        sourceBoxSizerOuter.Add(sourceBoxSizer, wx.SizerFlags(0).Expand().Border(wx.ALL, 10))
+        sourceBoxSizerOuter.Add(sourceBoxSizer, wx.SizerFlags(
+            0).Expand().Border(wx.ALL, 10))
         sourceBox.SetSizer(sourceBoxSizerOuter)
 
         footerSizer = wx.BoxSizer(wx.HORIZONTAL)
@@ -112,16 +122,20 @@ class SettingsFrame(wx.Frame):
         return dlg
 
     def showInvalidIntervalDialog(self):
-        wx.MessageDialog(self, 'Selecione um intervalo válido', 'Intervalo inválido').ShowModal()
+        wx.MessageDialog(self, 'Selecione um intervalo válido',
+                         'Intervalo inválido').ShowModal()
 
     def showInvalidDestDialog(self):
-        wx.MessageDialog(self, 'Selecione uma pasta de destino válida', 'Pasta de destino inválida').ShowModal()
+        wx.MessageDialog(self, 'Selecione uma pasta de destino válida',
+                         'Pasta de destino inválida').ShowModal()
 
     def showInvalidSourceDialog(self):
-        wx.MessageDialog(self, 'Selecione ao menos um arquivo para ser copiado', 'Arquivos inválidos').ShowModal()
+        wx.MessageDialog(self, 'Selecione ao menos um arquivo para ser copiado',
+                         'Arquivos inválidos').ShowModal()
 
     def showConfirmChangesErrorDialog(self):
-        wx.MessageDialog(self, 'Não foi possível salvar os dados', 'Error inesperado').ShowModal()
+        wx.MessageDialog(self, 'Não foi possível salvar os dados',
+                         'Error inesperado').ShowModal()
 
     def destroy(self):
         self.Destroy()
@@ -144,13 +158,15 @@ class SettingsPresenter:
 
     def _loadViewFromModel(self):
         if self._scheduleModel.copyInterval:
-            self._view.txtInterval.SetValue(str(self._scheduleModel.copyInterval))
+            self._view.txtInterval.SetValue(
+                str(self._scheduleModel.copyInterval))
 
         if self._copyModel.destination:
             self._view.txtDest.SetValue(self._copyModel.destination.as_posix())
 
         for source in self._copyModel.sources:
-            self._view.listSources.InsertItem(self._view.listSources.GetItemCount(), source.as_posix())
+            self._view.listSources.InsertItem(
+                self._view.listSources.GetItemCount(), source.as_posix())
 
     def selectDest(self):
         dialog = self._view.createDestDialog()
@@ -164,7 +180,8 @@ class SettingsPresenter:
 
     def insertSource(self, path):
         if path:
-            self._view.listSources.InsertItem(self._view.listSources.GetItemCount(), path)
+            self._view.listSources.InsertItem(
+                self._view.listSources.GetItemCount(), path)
 
     def removeSource(self):
         selectedItem = self._view.listSources.GetFirstSelected()
@@ -194,9 +211,11 @@ class SettingsPresenter:
 
     def _updateModel(self):
         try:
-            self._scheduleModel.setCopyInterval(self._view.txtInterval.GetValue())
+            self._scheduleModel.setCopyInterval(
+                self._view.txtInterval.GetValue())
             self._copyModel.setDestination(self._view.txtDest.GetValue())
-            sources = [self._view.listSources.GetItem(i, 0).GetText() for i in range(self._view.listSources.ItemCount)]
+            sources = [self._view.listSources.GetItem(i, 0).GetText(
+            ) for i in range(self._view.listSources.ItemCount)]
             self._copyModel.setSources(sources)
             self._schedulerManager.start()
             self.quit()
